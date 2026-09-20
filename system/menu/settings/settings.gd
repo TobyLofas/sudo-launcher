822d299ed2b5488bc8ca4de620713d9dd1354d48
Toby <33029609+TobyLofas@users.noreply.github.com>
2026-09-20 14:00:56 +0100

    Remove duplicate save_settings() calls

--- system/menu/settings/settings.gd
+++ system/menu/settings/settings.gd
@@ -12,11 +12,11 @@ func _on_list_mode_toggled(toggled_on: bool) -> void:
 
 func _on_open_to_last_toggled(toggled_on: bool) -> void:
 	Global.library_open_to_last_selected = toggled_on
-	Global.save_settings()
+	#Global.save_settings()
 
 func _on_show_icons_toggled(toggled_on: bool) -> void:
 	Global.library_display_images = toggled_on
-	Global.save_settings()
+	#Global.save_settings()
 
 func _on_visibility_changed() -> void:
 	%ListMode.button_pressed = Global.library_list_mode
